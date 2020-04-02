b15d9d997df1423a2217b1b653fdd1f8fa0345df
fathil11 <fathil.arham@gmail.com>
2020-04-02 14:56:25 +0700

    Change icon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,9 +3,10 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#00989b" />
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/logoMelawi.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/logoMelawi.png') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
