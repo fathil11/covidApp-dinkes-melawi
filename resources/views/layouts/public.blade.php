c286f3851b2a3fbd3fb2db7fbff94dc172bfcd10
fathil11 <fathil.arham@gmail.com>
2020-04-02 14:09:47 +0700

    Add favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,6 +3,8 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/logoMelawi.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/logoMelawi.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
