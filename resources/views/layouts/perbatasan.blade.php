5a4f83bcdd209ce00983503dd75722606bba991d
fathil11 <fathil.arham@gmail.com>
2020-04-30 09:22:59 +0700

    Perbatasan Admin icon add

--- resources/views/layouts/perbatasan.blade.php
+++ resources/views/layouts/perbatasan.blade.php
@@ -2,6 +2,12 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#00989b" />
+    <link rel="apple-touch-icon" sizes="76x76"
+        href="{{ asset('img/icon/apple-icon-180x180-dunplab-manifest-702.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/icon/apple-icon-180x180-dunplab-manifest-702.png') }}">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
