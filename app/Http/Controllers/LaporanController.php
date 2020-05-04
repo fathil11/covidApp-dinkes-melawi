ebe57e37b23b1a350e49bfaa5b803e55ac602de2
fathil11 <fathil.arham@gmail.com>
2020-05-04 19:04:46 +0700

    Laporan Internal test

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Exports\LaporanInternal;
 
 class LaporanController extends Controller
 {
     public function downloadHarian($pass)
     {
-        dd($pass);
+        return Excel::download(new LaporanHarianInternal, 'laporan.xlsx');
     }
 }
