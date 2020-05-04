58d7e2b22fe595655525bb5b71ef65e91e778da3
fathil11 <fathil.arham@gmail.com>
2020-05-04 19:09:37 +0700

    dep change

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Exports\LaporanHarianInternal;
 use Maatwebsite\Excel\Facades\Excel;
 
 class LaporanController extends Controller
