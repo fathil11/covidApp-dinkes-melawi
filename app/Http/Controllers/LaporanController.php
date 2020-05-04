1d54579b99e766b0264416cf54e9c1dd85f1a209
fathil11 <fathil.arham@gmail.com>
2020-05-04 19:05:24 +0700

    Fix laporan internal

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Exports\LaporanInternal;
 
 class LaporanController extends Controller
 {
     public function downloadHarian($pass)
     {
-        return Excel::download(new LaporanHarianInternal, 'laporan.xlsx');
+        return Excel::download(new App\Exports\LaporanHarianInternal, 'laporan.xlsx');
     }
 }
