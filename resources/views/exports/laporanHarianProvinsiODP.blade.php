92025049359629b2ca6dc2a751880eac6c6065b3
fathil11 <fathil.arham@gmail.com>
2020-05-18 13:08:36 +0700

    ODP Format

--- resources/views/exports/laporanHarianProvinsiODP.blade.php
+++ resources/views/exports/laporanHarianProvinsiODP.blade.php
@@ -2,36 +2,38 @@
     <thead>
         <tr>
             <th><b>No</b></th>
+            <th><b>Data Input</b></th>
             <th><b>Nama</b></th>
+            <th><b>Status</b></th>
             <th><b>Umur</b></th>
             <th><b>Jenis Kelamin</b></th>
+            <th><b>Kab/Kota</b></th>
             <th><b>Kecamatan</b></th>
             <th><b>Desa</b></th>
-            <th><b>Dusun</b></th>
-            <th><b>Nomor HP</b></th>
-            <th><b>Berkunjung Dari</b></th>
             <th><b>Kendaraan</b></th>
-            <th><b>Waktu Masuk</b></th>
-            <th><b>Gejala</b></th>
-            <th><b>Status</b></th>
+            <th><b>Berkunjung Dari</b></th>
+            <th><b>Lokasi Rawat</b></th>
+            <th><b>Lama Pantau</b></th>
+            <th><b>Tanggal PDP</b></th>
         </tr>
     </thead>
     <tbody>
         @foreach ($people as $key=>$person)
         <tr>
             <td>{{ $key+1 }}</td>
+            <td>{{ Carbon::create($person->created_at->toDateTimeString())->isoFormat('DD/MM/YY') }}</td>
             <td>{{ $person->name }}</td>
+            <td>ODP</td>
             <td>{{ $person->age }}</td>
-            <td>{{ ($person->gender == 'm') ? 'Laki-Laki' : 'Perempuan' }}</td>
+            <td>{{ ($person->gender == 'm') ? 'L' : 'P' }}</td>
+            <td>Melawi</td>
             <td>{{ $person->district }}</td>
             <td>{{ $person->village }}</td>
-            <td>{{ $person->sub_village }}</td>
-            <td>{{ $person->phone }}</td>
-            <td>{{ $person->track }}</td>
             <td>{{ $person->vehicle }}</td>
-            <td>{{ Carbon::create($person->created_at->toDateTimeString())->locale('id') }}</td>
-            <td>{{ $person->phenomenon }}</td>
-            <td>{{ $person->status }}</td>
+            <td>{{ $person->track }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
         @endforeach
     </tbody>
