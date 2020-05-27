a360875611269c511b8000ce2e64d272c14d9250
fathil11 <fathil.arham@gmail.com>
2020-05-27 18:41:58 +0700

    Push stat disable

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -14,7 +14,7 @@ class PublicController extends Controller
 {
     public function index()
     {
-        $this->pushStat();
+        // $this->pushStat();
 
         $stat['positive'] = Person::where('status', '5')->get()->count();
         $stat['recovered'] = Person::where('status', '7')->get()->count();
