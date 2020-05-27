dcad981d88b2ad079a999f5ea0a42ea07b193b98
fathil11 <fathil.arham@gmail.com>
2020-05-27 20:21:04 +0700

    Down tracing #1

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -14,7 +14,7 @@ class PublicController extends Controller
 {
     public function index()
     {
-        dd('Temporary Disabled');
+        // dd('Temporary Disabled');
 
         $this->pushStat();
 
